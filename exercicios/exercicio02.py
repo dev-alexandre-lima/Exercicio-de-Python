e9d9fc4daf8fce779a321235552a9675d08c8622
Alexandre Lima <dev.alexandre96@gmail.com>
2024-09-13 21:35:35 -0300

    fiz um programa que peça um número e mostre uma mensagem na tela

---
+++ exercicios/exercicio02.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+print('-=-' *10)
+
+num = int(input('Digite um valor: '))
+
+print('O valor que o usuário digitou foi {}.'.format(num))
