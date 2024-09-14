ddebb81f4dcee9c3d96dc3ee8bc5e33addc9c30c
Alexandre Lima <dev.alexandre96@gmail.com>
2024-09-13 21:44:37 -0300

    um programa que peça ao usuario dois valores e mostre a soma entre eles

---
+++ exercicios/exercicio03.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+valor_1 = int(input('Digite um valor: '))
+valor_2 = int(input('Digite outro valor: '))
+
+soma = valor_1 + valor_2
+
+print('A soma dos valores de {} e {} foi {}.'.format(valor_1, valor_2, soma))
